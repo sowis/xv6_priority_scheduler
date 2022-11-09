8191f3228cb3fe1b4306ba72104c7139fb22be65
진혁 박 <jh040597@naver.com>
2022-11-09 22:05:45 +0900

    shouter 유저 프로그램 추가

---
+++ xv6/shouter.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char* argv[])
+{
+    const int my_pid =  getpid();
+
+    while (1) {
+        printf(1, "pid %d, priority: %d\n", my_pid, get_proc_priority(my_pid));
+    }
+
+    return 0;
+}
